f82f710e907e9ed0ab2e39840b594345a407b88b
Erick Pires <pires@erickpires.com>
2017-05-12 23:25:03 -0300

    ELPA stuff.

---
+++ elpa/zenburn-0.1/zenburn-autoloads.el
@@ -0,0 +1,15 @@
+;;; zenburn-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil nil ("zenburn.el") (22790 4358 355211 705000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; zenburn-autoloads.el ends here
